19065e46bd097940d4f107880552e65358d6bedd
subhakani <43404568+subhakani@users.noreply.github.com>
2018-09-26 18:06:34 +0530

    Create u.py

---
+++ u.py
@@ -0,0 +1,5 @@
+lower=int(input("enter the lower limit for the range:"))
+upper=int(input("enter the upper limit for the range:"))
+for i in range(lower,upper=1):
+    if(i%2!=0):
+         print(i)    
