1b5dad3a297c6b610b82a6f5c5b28108bff9fa50
subhakani <43404568+subhakani@users.noreply.github.com>
2018-10-08 17:19:45 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,12 @@
+print("Enter 'x' for exit.");
+s1 = input("Enter first string to concatenate: ");
+if s1 == 'x':
+    exit();
+else:
+    s2 = input("Enter second string to concatenate: ");
+    s3 = s1 + s2;
+    print("\nString after concatenation =",s3);
+    print("S1 =",s1);
+    print("s2 =",s2);
+    print("S3 =",s3);
+
